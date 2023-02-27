415dffb307bf2188ce7a5712b0f93a971bb9feeb
VeregaS <sergeiusenkov56@gmail.com>
2023-02-27 17:35:42 +0500

    v1.1 with better optimization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
-import time
 import pyautogui
+import asyncio
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -18,51 +18,39 @@ cap.set(4, h)
 cords = {}
 pTime = 0
 last_status, hand_type = "", ""
+mouse_x = 0
+mouse_y = 0
 
-with mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.9, min_detection_confidence=0.9) as hands:
-    while True:
 
-        # ------------------------- Camera Load --------------------- #
-        success, frame = cap.read()
-        frame = cv2.flip(frame, 1)
-        # ----------------------------------------------------------- #
-        #############################################################
+def main():
+    global mouse_x, mouse_y
+    with mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.7, min_detection_confidence=0.7) as hands:
+        while True:
 
-        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = hands.process(imgRGB)
-        imgRGB = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2BGR)
+            # ------------------------- Camera Load --------------------- #
+            success, frame = cap.read()
+            frame = cv2.flip(frame, 1)
+            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
+            # ----------------------------------------------------------- #
 
-        if results.multi_hand_landmarks:
+            # ++++++++++++++++++++++ Main body +++++++++++++++++++++++++++++ #
+            if results.multi_hand_landmarks:
 
-            # +++++++++++++++++++++++++ Hand_Type +++++++++++++++++++++++++++ #
-            if results.multi_handedness:
-                hand_type = results.multi_handedness[0].classification[0].label
-            # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+                # ================== Finger Pos ======================== #
+                for hand_landmarks in results.multi_hand_landmarks:
+                    for id, lm in enumerate(hand_landmarks.landmark):
+                        h, w, c = frame.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        cords[f"{id}"] = cx, cy
+                # ====================================================== #
 
-            # ======================== Hand_Draw ============================ #
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(imgRGB, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(0, 0, 255)),
-                                          mp_drawing.DrawingSpec(color=(0, 0, 0)))
+                # _____________________Mouse____________________________ #
+                mouse_x, mouse_y = int(cords['8'][0]), int(cords['8'][1])
+                pyautogui.moveTo(mouse_x * cf_x, mouse_y * cf_y)
+                # ______________________________________________________ #
+            # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-            # =============================================================== #
 
-                # ----------------------- Hand_Cords ------------------------ #
-                for id, lm in enumerate(hand_landmarks.landmark):
-                    h, w, c = frame.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    cords[f"{id}"] = cx, cy
-                # ----------------------------------------------------------- #
-
-                # ######################## Hand_Status ###################### #
-                pyautogui.moveTo(int(cords['8'][0]) * cf_x, int(cords['8'][1]) * cf_y)
-                ###############################################################
-        else:
-            hand_type = "-"
-            last_status = "-"
-
-        if cv2.waitKey(1) == 27:
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    asyncio.run(main())
